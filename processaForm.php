fda5426780beacdeebaf861bf79e167f278faae7
Gustavo de Lima <gustavodelima1997@outlook.com>
2018-08-12 18:08:11 -0300

    Add files via upload

--- processaForm.php
+++ processaForm.php
@@ -1,26 +1,31 @@
 <?php
 
-$para= "gustavodelima1997@outlook.com"
-$nome= $_POST['nome'];
-$mail= $_POST['mail'];
-$tel= $_POST['tel'];
-$ass= $_POST['ass'];
-$msg= $_POST['msg'];
-
-  $corpo = "<strong> Mensagem de contato </strong><br/><br/>";
-  $corpo .= "<strong> Nome: </strong> $nome";
-  $corpo .= "<br/><strong> E-mail: </strong> $mail";
-  $corpo .= "<br/><strong> Telefone: </strong> $tel";
-  $corpo .= "<br/><strong> Assunto: </strong> $ass";
-  $corpo .= "<br/><strong> Mensagem: </strong> $msg";
-  
-  $header .= "Content-Type: text/html; charset= utf-8\n";
-  $header ="From: $mail Reply-to: $email\n";
-  
-@mail($para,$ass,$corpo,$header);
-
-header("location:contato.html?msg=enviado");
-
-$enviado = "<h1> Mensagem enviada, agradecemos seu contato!</h1>";
-  
+if (isset($_POST['BTEnvia'])) {
+	
+	$nome = $_POST['nome'];
+	$mail = $_POST['mail'];
+	$tel = $_POST['tel']; 
+	$ass = $_POST['ass'];
+	$mensagem = $_POST['msg'];
+	
+	$email_remetente = "contato@gustavodelima.com.br";
+	
+	$email_destinatario = "gustavodelima1997@outlook.com"; 
+	$email_reply = "$mail"; 
+	
+	$email_conteudo = "Nome = $nome \n"; 
+	$email_conteudo .= "Email = $mail \n";
+	$email_conteudo .= "Telefone = $telefone \n"; 
+	$email_conteudo .= "Assunto = $ass \n"; 
+	$email_conteudo .= "Mensagem = $mensagem \n"; 
+	
+	$email_headers = implode ( "\n",array ( "From: $email_remetente", "Reply-To: $email_reply", "Return-Path: $email_remetente","MIME-Version: 1.0","X-Priority: 3","Content-Type: text/html; charset=UTF-8" ) );
+	
+		if (mail ($email_destinatario, $ass, nl2br($email_conteudo), $email_headers)){ 
+					echo "</b>E-Mail enviado com sucesso!</b>"; 
+					
+					} 
+			else{ 
+					echo "</b>Falha no envio do E-Mail!</b>"; } 
+} 
 ?>
